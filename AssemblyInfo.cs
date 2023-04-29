06737d72db6ee08a305aeb9ab1e271d1b0cbef9a
Mahmoud Abduallah <69323993+Ma7moudAF@users.noreply.github.com>
2023-04-29 13:08:46 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// Assembly YAZSYS_POS, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyTitle("YAZSYS")]
+[assembly: System.Reflection.AssemblyDescription("Point Of Sale, YAZSYS Arabic")]
+[assembly: System.Reflection.AssemblyCompany("YAZSYS.com")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
+[assembly: System.Reflection.AssemblyProduct("YAZSYS")]
+[assembly: System.Resources.NeutralResourcesLanguage("")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: A.AssemblyInfo("")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.5", FrameworkDisplayName=".NET Framework 4.5")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2020")]
+[assembly: System.Runtime.InteropServices.Guid("96cf7289-6272-4b94-8810-846ed9e742d8")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+
